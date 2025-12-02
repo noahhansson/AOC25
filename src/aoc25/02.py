c96b58d3b34470e96ac2e89eee095218046cb8c4
Noah Hansson <hansson.noah@gmail.com>
2025-12-02 06:48:42 +0100

    Day 02

--- src/aoc25/02.py
+++ src/aoc25/02.py
@@ -19,6 +19,13 @@ def detect_dup_id_p1(p_id: str) -> bool:
         return True
     return False
 
+def detect_dup_id_p2(p_id: str) -> bool:
+    for idx in range(1, len(p_id) // 2 + 1):
+        pattern = rf"^(?:{p_id[:idx]})+$"
+        if len(re.findall(pattern, p_id[idx:])) > 0:
+            return True
+    return False
+
 
 @timer
 def get_first_solution(test: bool = False):
@@ -40,13 +47,25 @@ def get_first_solution(test: bool = False):
 
     return s
 
-
 @timer
 def get_second_solution(test: bool = False):
     inpt = parse_input(test)
 
-    return 0
+    invalid_ids = []
+
+    for id_range in inpt:
+        lo = int(id_range[0])
+        hi = int(id_range[1])
+
+        for p_id in range(lo, hi + 1):
+            if detect_dup_id_p2(str(p_id)):
+                invalid_ids.append(p_id)
+    s = 0
+    for p_id in invalid_ids:
+        s += p_id
+
+    return s
 
 
 print(f"P1: {get_first_solution(test=args.test)}")
-# print(f"P2: {get_second_solution(test=args.test)}")
+print(f"P2: {get_second_solution(test=args.test)}")
