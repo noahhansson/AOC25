55e6839b7c544de471c81170e0282fafc2106470
Noah Hansson <hansson.noah@gmail.com>
2025-12-03 06:50:36 +0100

    Day 03 P2

--- src/aoc25/03.py
+++ src/aoc25/03.py
@@ -5,8 +5,21 @@ args = setup_args()
 
 def parse_input(test: bool = False):
     inpt = read_input("03", test=test)
-    return [[int(x) for x in bank] for bank in inpt]
+    return [tuple(int(x) for x in bank) for bank in inpt]
 
+def solve(bank: tuple[int, ...], num_to_enable: int) -> int:
+    if num_to_enable==0:
+        return 0
+    max_idx = -1
+    max_digit = -1
+    for i, battery in enumerate(bank):
+        if battery > max_digit:
+            max_idx = i
+            max_digit = battery
+        if (battery == 9) or ((i + num_to_enable) >= len(bank)):
+            break
+
+    return int((10 ** (num_to_enable - 1)) * bank[max_idx] + solve(bank[max_idx+1:], num_to_enable-1))
 
 @timer
 def get_first_solution(test: bool = False):
@@ -15,38 +28,20 @@ def get_first_solution(test: bool = False):
     joltage_sum = 0
 
     for bank in banks:
-        idx_first = -1
-        idx_second = -1
-
-        max_digit = -1
-        for i, battery in enumerate(bank[:-1]):
-            if battery > max_digit:
-                idx_first = i
-                max_digit = battery
-            if battery == 9:
-                break
-        
-        max_digit = -1
-        for i, battery in enumerate(bank[idx_first+1:], start=idx_first+1):
-            if battery > max_digit:
-                idx_second = i
-                max_digit = battery
-            if battery == 9:
-                break
-
-        joltage = 10 * bank[idx_first] + bank[idx_second]
-
-        joltage_sum+= joltage
-
+        joltage = solve(bank, 2)
+        joltage_sum += joltage
     return joltage_sum
 
 
 @timer
 def get_second_solution(test: bool = False):
-    inpt = parse_input(test)
-
-    return 0
+    banks = parse_input(test)
+    joltage_sum = 0
+    for bank in banks:
+        joltage = solve(bank, 12)
+        joltage_sum += joltage
+    return joltage_sum
 
 
 print(f"P1: {get_first_solution(test=args.test)}")
-# print(f"P2: {get_second_solution(test=args.test)}")
+print(f"P2: {get_second_solution(test=args.test)}")
